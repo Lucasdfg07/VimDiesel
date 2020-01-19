baea3ac65c7b4a9c360b4792b88ad0d583df9e6f
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2020-01-19 03:07:54 -0300

    alteração projeto final

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require tether/dist/js/tether
 //= require bootstrap/dist/js/bootstrap.bundle
 //= require rails-ujs
 //= require activestorage
